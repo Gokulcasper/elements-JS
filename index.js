74a1327ff39c724183cec2fca985dff3c3429b64
Gokulcasper <ronitcasper215@gmail.com>
2024-07-09 09:41:34 +0530

    finding Array elements

---
+++ index.js
@@ -0,0 +1,6 @@
+const item=["apple","grapes","mango","lemon","apple"]
+
+// console.log(item.indexOf("mango"));
+// console.log(item.lastIndexOf("apple"));
+console.log((item.indexOf("red")!== -1));
+
